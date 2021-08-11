2263255025ddfebff7f40d33212afd95e1c1fec8
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2021-08-11 17:37:00 +0200

    Add Noticeable Newspage embed Widget on first page

--- src/Page1.js
+++ src/Page1.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import {config} from './config';
+
+
 export default class Page1 extends React.Component {
 
+    componentDidMount() {
+        window.noticeable.render('widget', config.noticeable.newspageEmbedWidgetId, {selector: '#noticeable-newspage-embed'});
+    }
+
+    componentWillUnmount() {
+        window.noticeable.destroy('widget', config.noticeable.newspageEmbedWidgetId);
+    }
+
     render() {
-        return <div>Page 1</div>;
+        return <div id="noticeable-newspage-embed" style={{width: '100%'}}></div>;
     }
 
 }
